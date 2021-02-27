dfeede4e4db9a9ae3ac14d22152801e6d74c903b
Nico Rithner <nicorithner@gmail.com>
2021-02-27 13:33:52 -0700

    Add video validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,16 @@
 class Video < ApplicationRecord
   has_one_attached :clip
   has_one_attached :thumbnail
+  validates :title, presence: true
+  validate :correct_video_type
+
+  private
+
+  def correct_video_type
+    if clip.attached? && !clip.content_type.in?(%w(clip/mp4))
+      errors.add(:clip, "must be a mp4")
+    elsif clip.attached? == false
+      errors.add(:clip, "required")
+    end
+  end
 end
